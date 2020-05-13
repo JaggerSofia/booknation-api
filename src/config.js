41d79148ceae1b066e404eb8848ed2cd403f5495
Victoria Moore <victoriamoore@Victorias-MacBook-Pro.local>
2020-05-13 15:35:02 -0700

    new innerjoin added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'https://shrouded-wave-72208.herokuapp.com/',
+  DATABASE_URL: process.env.DATABASE_URL || 'http://localhost:8000/api',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Victoria@localhost/booknation-test',
   JWT_SECRET: process.env.JWT_SECRET || 'cat',
 }
