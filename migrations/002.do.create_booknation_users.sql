75e9533045a8024d32f6910a79bda06aa4b1adcb
Victoria Moore <victoriamoore@Victorias-MacBook-Pro.local>
2020-05-16 15:22:50 -0700

    readding nickname

--- migrations/002.do.create_booknation_users.sql
+++ migrations/002.do.create_booknation_users.sql
@@ -3,6 +3,7 @@ CREATE TABLE booknation_users (
   user_name TEXT NOT NULL UNIQUE,
   full_name TEXT NOT NULL,
   password TEXT NOT NULL,
+  nickname TEXT,
   date_created TIMESTAMP NOT NULL DEFAULT now(),
   date_modified TIMESTAMP
 );
